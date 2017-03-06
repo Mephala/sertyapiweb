2c36c86231a9cbab4e629d1f2b47749ccceb0b98
mephala <gokhan.ozgozen@gmail.com>
2017-03-06 12:11:29 +0300

    fileDl and mavenrun tomcat addition.

--- sertyapiWeb/src/main/java/com/sertyapi/controller/HomeController.java
+++ sertyapiWeb/src/main/java/com/sertyapi/controller/HomeController.java
@@ -1,19 +1,34 @@
 package com.sertyapi.controller;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
+import com.sertyapi.model.Model;
+import org.apache.commons.io.IOUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.sertyapi.model.Model;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 @Controller
 public class HomeController extends BaseController {
 
-	@RequestMapping(value = "/anasayfa")
-	public Object showHomePage(HttpServletRequest request, HttpServletResponse response) {
-		return createModel(request, response, Model.HOME);
-	}
+    @RequestMapping(value = "/anasayfa")
+    public Object showHomePage(HttpServletRequest request, HttpServletResponse response) {
+        return createModel(request, response, Model.HOME);
+    }
+
+
+    @RequestMapping(value = "/ezgim")
+    public void ezgim(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        InputStream fileStream = this.getClass().getClassLoader().getResourceAsStream("abuzittin");
+        String headerKey = "Content-Disposition";
+        String headerValue = String.format("attachment; filename=\"%s\"", "abuzittin");
+        response.setHeader(headerKey, headerValue);
+        OutputStream outStream = response.getOutputStream();
+        IOUtils.copy(fileStream, outStream);
+        response.flushBuffer();
+    }
 
 }
